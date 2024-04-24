c4121250843242a9dae139d80f883f914cdebecd
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-04-24 12:24:28 +0100

    Tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = async (firstName, lastName, fileName) => {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  try {
-    const [user, photo] = await Promise.allSettled([userPromise, photoPromise]);
-
-    return [
-      { status: user.status, value: user.value },
-      { status: photo.status, value: photo.value },
-    ];
-  } catch (error) {
-    return [{ status: 'rejected', value: error }];
-  }
-};
-
-export default handleProfileSignup;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const result = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) => result.push({
+      status,
+      value: status === 'rejected' ? reason.toString() : value,
+    }));
+    return result;
+  });
+}
