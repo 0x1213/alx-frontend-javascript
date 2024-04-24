58af0b5bd71760683f603ead0a05437b4a988761
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-04-24 12:17:35 +0100

    Tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((data) => {
-    console.log(`${data}`);
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
+    (data) => {
+      const arr = [];
+      for (const item of data) {
+        arr.push({ status: item.status, value: item.value || item.reason });
+      }
+      return arr;
+    },
+  );
 }
