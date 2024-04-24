19f0fca54d8fb3cd6db76b734018395fc1583099
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-04-24 11:54:04 +0100

    Tasks

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,10 @@
+export default function uploadPhoto(filename) {
+  return new Promise((resolve, reject) => {
+    const success = false;
+    if (success) {
+      resolve();
+    } else {
+      reject(new Error(`${filename} cannot be processed`));
+    }
+  });
+}
