bb3e1cc621918e9b20f9d7b30a2d7a82dae559a0
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-04-29 17:02:39 +0100

    Tasks

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const dv = new DataView(buffer);
-  dv.setInt8(position, value);
-  return dv;
+  try {
+    const buffer = new ArrayBuffer(length);
+    const dv = new DataView(buffer);
+    dv.setInt8(position, value);
+    return dv;
+  } catch (err) {
+    throw Error('Position outside range');
+  }
 }
