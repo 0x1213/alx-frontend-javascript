612853cb242196bd7fec063fa96cc8bf3a5ac524
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-04-30 20:22:59 +0100

    Tasks

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,15 @@
 export default function cleanSet(set, startString) {
+  const arr = [];
+
   if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
-  const tmp = [];
+
   for (const e of set) {
-    if (e.startsWith(startString)) {
-      tmp.push(e.replace(startString, ''));
+    if (e && e.startsWith(startString)) {
+      arr.push(e.slice(startString.length));
     }
   }
-  return tmp.join('-');
+
+  return arr.join('-');
 }
